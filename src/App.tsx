235296675a910d8c2f5502b2ef8ca698da9b3fc0
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-08-14 11:53:29 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import ArraySample from "./jsx/ArraySample"
 import IfElseSample from "./jsx/IfElseSample"
 import JsxIntro from "./jsx/JsxIntro"
+import ObjectArraySample from "./jsx/ObjectArraySample"
 import ObjectSample from "./jsx/ObjectSample"
 
 function App() {
   return <>
-  <ArraySample/>
+  <ObjectArraySample/>
   </>
 }
 
